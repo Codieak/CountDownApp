f41dccd46849980163a143cdb561edc2beffd055
Codieak <46133623+Codieak@users.noreply.github.com>
2020-11-07 14:48:09 +0530

    Date Changed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const newYear="1 Jan 2021";
+const newYear="26 Mar 2021";
 function countdown()
 {
     const newYearDate=new Date(newYear);
