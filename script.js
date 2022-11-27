fd714d3e35463c719545cf321998375714cde538
Codieak <46133623+Codieak@users.noreply.github.com>
2022-11-27 19:27:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
 const secondsEl = document.getElementById('seconds');
 
-const newYear="26 Mar 2021";
+const newYear="13 Feb 2023";
 function countdown()
 {
     const newYearDate=new Date(newYear);
